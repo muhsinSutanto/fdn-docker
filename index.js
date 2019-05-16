72c0704c5668510ac0e9e67acc4c3463b40b8e04
FDN-Raditya <fdn-raditya@Nurs-MacBook-Pro.local>
2019-05-16 10:44:22 +0700

    Created nodejs app

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const redis = require('redis')
+
+const redisClient = redis.createClient({
+    host: "redis-server",
+    port: 6379
+})
+
+const app = express()
+
+redisClient.set("visits", 0)
+
+app.get("/", (req, res) => {
+    redisClient.get("visits", (err, visitCount) => {
+        if (err) {
+            console.log("Error: ", err)
+        }
+
+        res.status(200).send("Number of visits: ", + visitCount)
+        redisClient.set("visists", parseInt(visitCount) + 1)
+    })
+})
+
+app.listen(4000, () => {
+    console.log("running on port :4000")
+})
